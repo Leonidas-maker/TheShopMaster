1bfb52b37b08f536fefbecbd21ae65ec53cad6a6
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2023-11-30 16:46:53 +0100

    change penny crawler

--- Sonstiges/crawler/penny/penny.py
+++ Sonstiges/crawler/penny/penny.py
@@ -20,9 +20,16 @@ url = f"https://www.penny.de/.rest/offers/{year}-{week}"
 response =  requests.get(url)
 data = json.loads(response.text)
 
-# Get the length of the categories
-lenCat = len(data[0]["categories"])
+categories = data[0]["categories"]
 
-for categories in data[0]["categories"]:
-    for offerTiles in categories["offerTiles"]:
-        print(offerTiles.get("title"))
+#TODO productData need to be parsed
+for category in categories:
+    for article in category["offerTiles"]:
+        product_name = article.get("title")
+        if product_name:
+            product_price = article.get("price")
+            product_quantity = article.get("quantity")
+            product_originalPrice = article.get("originalPrice")
+            product_originalPriceType = article.get("originalPriceType")
+            product =(product_name, product_price, product_quantity, product_originalPrice, product_originalPriceType)  
+            print(product)
