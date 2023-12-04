34e17be503117ae111ba605b852fc5e9b3a2232e
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-12-04 15:42:37 +0100

    trying nativewind

--- Client/src/pages/loading/loading.tsx
+++ Client/src/pages/loading/loading.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Text, View } from "react-native";
+import { Text, View, Pressable, Button } from "react-native";
+import { styled, useColorScheme } from "nativewind";
 
-import Registration from "../registration/registration";
+import { StyledComponent } from "nativewind";
+
+import Registration from '../registration/registration';
 import Login from "../login/login";
 import Dashboard from "../dashboard/dashboard";
 import LoadingSVG from "../../components/svg/loadingSVG";
+
+const StyledPressable = styled(Button)
+const StyledText = styled(Text)
 
 const Loading = (props:any) => {
     const { navigation } = props;
@@ -21,7 +27,8 @@ This is just a menu to test some basic functions:
 
 `}
 <LoadingSVG />
-<button type="button" onClick={() => navigation.navigate(Registration)} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registration</button>
+<StyledPressable title="Registration" className="text-black font-bold" onPress={() => navigation.navigate(Registration)}></StyledPressable>
+
 {`
 
 `}
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,6 +1,8 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/App.{js,jsx,ts,tsx}",
     "./src//*.{js,jsx,ts,tsx}",
     "./src/components/svg/*.{js,jsx,ts,tsx}"
   ],
