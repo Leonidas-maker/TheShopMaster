9ac9517a1fddc92636d142dc49f8cfd0c9675a0c
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2023-12-25 02:25:25 +0100

    Added Create_User API

--- server/main.py
+++ server/main.py
@@ -1,13 +1,16 @@
 
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.absolute() / "config"))
+sys.path.append(str(Path(__file__).parent.parent.absolute() / "controllers"))
 sys.path.append(str(Path(__file__).parent.parent.absolute() / "models"))
 
+from sqlalchemy.orm import Session
+from controllers import user_crud
 from models import m_user, s_user
 from database import SessionLocal, engine
 
@@ -22,19 +25,15 @@ def get_db():
     finally:
         db.close()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.post("/users/", response_model=s_user.User)
+def create_user(user: s_user.UserCreate, db: Session = Depends(get_db)):
+    db_user = user_crud.get_user_by_email_or_username(db, user.email, user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return user_crud.create_user(db = db, user = user)
--- server/models/s_user.py
+++ server/models/s_user.py
@@ -1,14 +1,135 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
 
-class UserBase(BaseModel):
-    email: str
+# Pydantic models
 
-class UserCreate(UserBase):
-    password: str
+class AddressBase(BaseModel):
+    address1: str
+    address2: Optional[str] = None
+    district: str
+    postal_code: str
+    
 
-class User(UserBase):
-    id: int
-    is_active: bool
+class Address(AddressBase):
+    address_id: int
+    city_id: int
+    last_modified: datetime
 
     class Config:
         from_attributes = True
+
+class CityBase(BaseModel):
+    city: str
+    country_id: int
+
+class CityCreate(CityBase):
+    pass
+
+class City(CityBase):
+    city_id: int
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class CountryBase(BaseModel):
+    country: str
+
+class CountryCreate(CountryBase):
+    pass
+
+class Country(CountryBase):
+    country_id: int
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: Optional[bool] = True
+
+class User(UserBase):
+    user_id: int
+    avatar: Optional[bytes] = None
+    address_id: Optional[int] = None
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class UserSecurityBase(BaseModel):
+    password: str
+    _2fa: Optional[bool] = False
+    verified: Optional[bool] = False
+
+class UserSecurity(UserSecurityBase):
+    user_id: int
+    security_id: int
+    forgot_password: Optional[str] = None
+    application_token: Optional[dict] = None
+    _2fa_backup: Optional[str] = None
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class ShoppingCartBase(BaseModel):
+    user_id: int
+    product_id: int
+    quantity: int
+
+class ShoppingCart(ShoppingCartBase):
+    shoppingCart_id: int
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class FavoriteProductBase(BaseModel):
+    user_id: int
+    product_id: int
+
+class FavoriteProductCreate(FavoriteProductBase):
+    pass
+
+class FavoriteProduct(FavoriteProductBase):
+    favoriteProduct_id: int
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: int
+
+class Product(ProductBase):
+    product_id: int
+    last_modified: datetime
+
+    class Config:
+        from_attributes = True
+
+
+# Pydantic create-models 
+
+class AddressCreate(AddressBase):
+    city: str
+    country: str
+
+class UserSecurityCreate(UserSecurityBase):
+    pass
+
+class UserCreate(UserBase):
+    security: UserSecurityCreate
+    address: Optional[AddressCreate] = None
+
+class ShoppingCartCreate(ShoppingCartBase):
+    pass
+
+class ProductCreate(ProductBase):
+    pass
